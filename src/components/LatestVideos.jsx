7350eaff37b7a33d1751978ec83043821eef73c5
Dev1672000 <dev1672000@gmail.com>
2024-01-26 15:19:50 +0530

    aad

--- src/components/LatestVideos.jsx
+++ src/components/LatestVideos.jsx
@@ -8,8 +8,8 @@ const LatestVideos = () => {
   };
 
   return (
-    <div>
-      <div className="md:ml-[11.5%] mx-5 my-[5%] ">
+    <div className="md:mx-[11.5%] mx-5">
+      <div className=" my-[5%] ">
         <h1 className="text-[90px] text-[#f3f3f3] uppercase font-[700]">
           latest
         </h1>
@@ -17,19 +17,19 @@ const LatestVideos = () => {
           Videos
         </h1>
       </div>
-      <div className="md:mx-[12%] mx-5 md:flex justify-center items-center">
-        <div className="">
+      <div className=" mx-5  md:flex justify-center items-center">
+        <div className="md:w-[70%] h-96">
           <iframe
-            className="md:w-[727px] md:h-[408px] h-80 w-full"
+            className="w-full h-full"
             src={`https://www.youtube.com/embed/${selectedVideo}?autoplay=1`}
             title="YouTube video player"
             allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
-            muted 
-          ></iframe>
+            muted
+          />
         </div>
-        <div className="">
-          <ul className="bg-black md:w-[80%] px-5 h-full space-y-4 text-white list-decimal p-3">
+        <div className="md:w-[30%] h-96 overflow-hidden">
+          <ul className="bg-black h-full w-full  px-5 text-sm  space-y-4 text-white list-decimal p-3">
             <li onClick={() => handleVideoClick("yuSJbpXfv1I")}>
               Rising waters submerge cars and cause traffic jam on the M1
               freeway in Port
